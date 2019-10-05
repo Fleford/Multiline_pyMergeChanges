e3fedea07dd5d013179b29c9efb524c625e296a7
Fleford <fleford@gmail.com>
2019-10-04 21:52:32 -0600

    started working on function

--- Better_pyMergeChanges.py
+++ Better_pyMergeChanges.py
@@ -3,7 +3,6 @@ import re
 line_cnt = 0
 with open('test.tex', 'r') as f:
     f_string = f.read()
-print(repr(f_string))
 
     # for line in f:
     #     line_cnt = line_cnt + 1
@@ -14,4 +13,22 @@ print(repr(f_string))
     #     #         print(line_cnt, char_cnt, repr(char))
     #
     #     print(line_cnt, line.find(r"\deleted["))
+print(repr(f_string))
+
+text = 'aaaa(bb()()ccc)dd'
+istart = []  # stack of indices of opening parentheses
+d = {}
+
+for i, c in enumerate(f_string):
+    if c == '{':
+         istart.append(i)
+    if c == '}':
+        try:
+            d[istart.pop()] = i
+        except IndexError:
+            print('Too many closing parentheses')
+if istart:  # check if stack is empty afterwards
+    print('Too many opening parentheses')
+print(d)
+print(repr(f_string[16: 696]))
 
