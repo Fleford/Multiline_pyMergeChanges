97193de89eb901afeb099d2103ada5e2d0c06ebc
fleford <fleford@gmail,com>
2019-10-11 20:52:01 -0600

    iteratively adds \\added

--- Better_pyMergeChanges.py
+++ Better_pyMergeChanges.py
@@ -50,41 +50,35 @@ line_cnt = 0
 with open('test.tex', 'r') as f:
     f_string = f.read()
 
-    # for line in f:
-    #     line_cnt = line_cnt + 1
-    #     # char_cnt = 0
-    #     # for char in line:
-    #     #     char_cnt = char_cnt + 1
-    #     #     if char == "\\":
-    #     #         print(line_cnt, char_cnt, repr(char))
-    #
-    #     print(line_cnt, line.find(r"\deleted["))
-# print(repr(f_string))
-#
-# d_curly_bracket = find_curly_brackets(f_string)
-#
-# print("Curly bracket")
-# print(d_curly_bracket)
-#
-# d_square_bracket = find_square_brackets(f_string)
-#
-# print("Square bracket")
-# print(d_square_bracket)
-
-print()
+# Start merging changes
 edit_found = True
 while edit_found:
     edit_found = False
+
+    # Apply first \deleted found
     if not f_string.find('\\deleted') == -1:
         edit_found = True
         d_curly_bracket = find_curly_brackets(f_string)
         d_square_bracket = find_square_brackets(f_string)
-        flag_index = f_string.find('\\deleted')
-        left_square_bracket = flag_index + 8
+        deleted_start_index = f_string.find('\\deleted')
+        left_square_bracket = deleted_start_index + 8
         right_square_bracket = d_square_bracket[left_square_bracket]
         left_curly_bracket = right_square_bracket + 1
         right_curly_bracket = d_curly_bracket[left_curly_bracket]
-        f_string = f_string[:flag_index] + f_string[right_curly_bracket + 1:]
+        f_string = f_string[:deleted_start_index] + f_string[right_curly_bracket + 1:]
+
+    # Apply first \added found
+    if not f_string.find('\\added') == -1:
+        edit_found = True
+        d_curly_bracket = find_curly_brackets(f_string)
+        d_square_bracket = find_square_brackets(f_string)
+        added_start_index = f_string.find('\\added')
+        left_square_bracket = added_start_index + 6
+        right_square_bracket = d_square_bracket[left_square_bracket]
+        left_curly_bracket = right_square_bracket + 1
+        right_curly_bracket = d_curly_bracket[left_curly_bracket]
+        f_string = f_string[:added_start_index] + f_string[left_curly_bracket + 1:right_curly_bracket] + \
+            f_string[right_curly_bracket + 1:]
 
 with open('test_merge.tex', 'w') as f_out:
     f_out.write(f_string)
